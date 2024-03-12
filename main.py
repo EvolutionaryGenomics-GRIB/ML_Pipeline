9f2b69b7efe2bed8c18e2f8f6154b8d40a25b241
sergiovmdo <sergiovazquezmontesdeoca@gmail.com>
2024-03-12 10:50:59 +0100

    Code refactor for exception control.

--- main.py
+++ main.py
@@ -3,62 +3,78 @@ import pandas as pd
 import time
 import json
 import copy
+import re
 
 from model.ModelPipeline import ModelPipeline
 from preprocessing.PreprocessingPipeline import PreprocessingPipeline
+import matplotlib.colors as mcolors
 
 
 def exception_control(parameters):
-    valid_parameters = ['target', 'features', 'scaler', 'encoder', 'class_balancer', 'evaluation_technique',
-                        'model', 'enable_parameter_search', 'splitting_runs', 'bootstrap_runs', 'output_file',
-                        'num_features', 'feature_selector', 'parameters_grid', 'plot_mean_roc', 'roc_color']
+    """
+    Checks that all the given parameters have the correct type and are accepted by the framework. Raises an
+    exception with a descriptive message in case anything is not correct.
 
-    valid_scalers = ['min_max', 'z_score']
-    valid_encoders = ['one_hot', 'target_encoding']
-    valid_balancers = ['smote', 'random_oversampling']
-    valid_evaluation_techniques = ['train_test', 'bootstrap', '.632+']
-    valid_models = ['logistic_regression', 'random_forest', 'xgboost', 'rbf_svm', 'gradient_descent']
+    Args:
+        parameters (dict): dictionary containing all the parameters of the model.
+
+    """
+    valid_parameters = {
+        'target': {'type': str, 'error_msg': 'Parameter "{}" has to be a string'},
+        'features': {'type': list, 'error_msg': 'Parameter "{}" has to be a list or the empty string'},
+        'scaler': {'valid_values': ['min_max', 'z_score'],
+                   'error_msg': 'The scaler is not available, the implemented scalers keys are: {}'},
+        'encoder': {'valid_values': ['one_hot', 'target_encoding'],
+                    'error_msg': 'The encoder is not available, the implemented encoders keys are: {}'},
+        'class_balancer': {'valid_values': ['smote', 'random_oversampling'],
+                           'error_msg': 'The balancer is not available, the implemented balancer keys are: {}'},
+        'evaluation_technique': {'valid_values': ['train_test', 'bootstrap', '.632+'],
+                                 'error_msg': 'The evaluation is not available, the implemented evaluation keys are: {}'},
+        'model': {'valid_values': ['logistic_regression', 'random_forest', 'xgboost', 'rbf_svm', 'gradient_descent'],
+                  'error_msg': 'The model is not available, the implemented model keys are: {}'},
+        'enable_parameter_search': {'type': bool, 'error_msg': 'Parameter "{}" has to be a boolean'},
+        'splitting_runs': {'type': int, 'error_msg': 'Parameter "{}" has to be an integer'},
+        'bootstrap_runs': {'type': int, 'error_msg': 'Parameter "{}" has to be an integer'},
+        'output_file': {'type': str, 'error_msg': 'Parameter "{}" has to be a string'},
+        'num_features': {'type': int, 'error_msg': 'Parameter "{}" has to be an integer'},
+        'feature_selector': {'type': str, 'error_msg': 'Parameter "{}" has to be a string'},
+        'parameters_grid': {'valid_types': [str, dict], 'error_msg': 'Parameter "{}" has to be a string'},
+        'plot_mean_roc': {'type': bool, 'error_msg': 'Parameter "{}" has to be a boolean'},
+        'roc_color': {'type': str, 'error_msg': 'The given color is not correct'}
+    }
 
     for k, v in parameters.items():
         if k not in valid_parameters:
             raise ValueError("This parameter name is not valid: {}".format(k))
 
-        if k == valid_parameters[0]:
-            if not isinstance(v, str):
-                raise TypeError("Parameter: {} has to be a string".format(str(k)))
-        elif k == valid_parameters[1]:
-            if not isinstance(v, list) and v != '':
-                raise TypeError("Parameter: {} has to be a list or the empty string".format(str(k)))
-        elif k == valid_parameters[2]:
-            if v not in valid_scalers and v != '':
-                raise ValueError("The scaler is not available, the implemented scalers keys are: {}".format(", ".join(valid_scalers)))
-        elif k == valid_parameters[3]:
-            if v not in valid_encoders and v != '':
-                raise ValueError("The encoder is not available, the implemented encoders keys are: {}".format(", ".join(valid_encoders)))
-        elif k == valid_parameters[4]:
-            if v not in valid_balancers and v != '':
-                raise ValueError("The balancer is not available, the implemented balancer keys are: {}".format(", ".join(valid_balancers)))
-        elif k == valid_parameters[5]:
-            if v not in valid_evaluation_techniques and v != '':
-                raise ValueError("The evaluation is not available, the implemented evaluation keys are: {}".format(", ".join(valid_evaluation_techniques)))
-        elif k == valid_parameters[6]:
-            if v not in valid_models and v != '':
-                raise ValueError("The model is not available, the implemented model keys are: {}".format(", ".join(valid_models)))
-        elif k == valid_parameters[7]:
-            if not isinstance(v, bool):
-                raise TypeError("Parameter: {} has to be a boolean".format(str(k)))
-        elif k == valid_parameters[8]:
-            if not isinstance(v, int):
-                raise TypeError("Parameter: {} has to be an integer".format(str(k)))
-        elif k == valid_parameters[9]:
-            if not isinstance(v, int):
-                raise TypeError("Parameter: {} has to be an integer".format(str(k)))
-        elif k == valid_parameters[10]:
-            if not isinstance(v, str):
-                raise TypeError("Parameter: {} has to be a string".format(str(k)))
+        if 'type' in valid_parameters[k] and not isinstance(v, valid_parameters[k]['type']):
+            raise TypeError(valid_parameters[k]['error_msg'].format(k))
+
+        if 'valid_types' in valid_parameters[k] and not any(
+                isinstance(v, t) for t in valid_parameters[k]['valid_types']):
+            raise TypeError(valid_parameters[k]['error_msg'].format(k))
+
+        if 'valid_values' in valid_parameters[k]:
+            valid_values = valid_parameters[k]['valid_values']
+            if v not in valid_values and v != "":
+                raise ValueError(valid_parameters[k]['error_msg'].format(", ".join(valid_values)))
+
+        if k == 'roc_color':
+            if (v not in list(mcolors.CSS4_COLORS.keys())) and (not bool(re.match(r'^#[0-9a-fA-F]{6}$', v))):
+                raise ValueError(valid_parameters[k]['error_msg'])
 
 
 def generate_combinations(json_obj, current_combination, combinations_list):
+    """
+    In case any of the parameters has multiple options it generates all the possible combination between the different
+    parameters so the pipeling is run as many times as combinations exist.
+
+    Args:
+        json_obj (dict): JSON object containing all the parameters.
+        current_combination (dict): already converted dictionary from the JSON file.
+        combinations_list (dict): list of converted dictionaries.
+    """
+
     if not json_obj:
         combinations_list.append(current_combination)
         return
